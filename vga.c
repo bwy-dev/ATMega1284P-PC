9a4965b68c7b286ee55f430146df660e2dabd8b0
bwy-webd <bwordsworthyates@gmail.com>
2020-03-05 12:13:52 +0000

    fixed reset issue due to unhandled interrupt

--- vga.c
+++ vga.c
@@ -66,117 +66,88 @@ void vga_move_cursor()
 
 void vga_add_char_at_cursor(char b)
 {
-	
-    volatile char c;
     switch (b)
     {
       case KEY_A:
           vga_add_char_to_frame_buffer(CHAR_A_UP);
-          c = 'A';
           break;
       case KEY_B:
           vga_add_char_to_frame_buffer(CHAR_B_UP);
-		  c = 'B';
           break;
       case KEY_C:
           vga_add_char_to_frame_buffer(CHAR_C_UP);
-          c = 'C';
           break;
       case KEY_D:
           vga_add_char_to_frame_buffer(CHAR_D_UP);
-          c = 'D';
           break;
       case KEY_E:
           vga_add_char_to_frame_buffer(CHAR_E_UP);
-          c = 'E';
           break;
       case KEY_F:
           vga_add_char_to_frame_buffer(CHAR_F_UP);
-          c = 'F';
           break;
       case KEY_G:
           vga_add_char_to_frame_buffer(CHAR_G_UP);
-          c = 'G';
           break;
       case KEY_H:
           vga_add_char_to_frame_buffer(CHAR_H_UP);
-          c = 'H';
           break;
       case KEY_I:
           vga_add_char_to_frame_buffer(CHAR_I_UP);
-          c = 'I';
           break;
       case KEY_J:
           vga_add_char_to_frame_buffer(CHAR_J_UP);
-          c = 'J';
           break;
       case KEY_K:
           vga_add_char_to_frame_buffer(CHAR_K_UP);
-          c = 'K';
           break;
       case KEY_L:
           vga_add_char_to_frame_buffer(CHAR_L_UP);
-          c = 'L';
           break;
       case KEY_M:
           vga_add_char_to_frame_buffer(CHAR_M_UP);
-          c = 'M';
           break;
       case KEY_N:
           vga_add_char_to_frame_buffer(CHAR_N_UP);
-          c = 'N';
           break;
       case KEY_O:
           vga_add_char_to_frame_buffer(CHAR_O_UP);
-          c = 'O';
           break;
       case KEY_P:
           vga_add_char_to_frame_buffer(CHAR_P_UP);
-          c = 'P';
           break;
       case KEY_Q:
           vga_add_char_to_frame_buffer(CHAR_Q_UP);
-          c = 'Q';
           break;
       case KEY_R:
           vga_add_char_to_frame_buffer(CHAR_R_UP);
-          c = 'R';
           break;
       case KEY_S:
           vga_add_char_to_frame_buffer(CHAR_S_UP);
-          c = 'S';
           break;
       case KEY_T:
           vga_add_char_to_frame_buffer(CHAR_T_UP);
-          c = 'T';
           break;
       case KEY_U:
           vga_add_char_to_frame_buffer(CHAR_U_UP);
-          c = 'U';
           break;
       case KEY_V:
           vga_add_char_to_frame_buffer(CHAR_V_UP);
-          c = 'V';
           break;
       case KEY_W:
           vga_add_char_to_frame_buffer(CHAR_W_UP);
-          c = 'W';
           break;
       case KEY_X:
           vga_add_char_to_frame_buffer(CHAR_X_UP);
-          c = 'X';
           break;
       case KEY_Y:
           vga_add_char_to_frame_buffer(CHAR_Y_UP);
-          c = 'Y';
           break;
       case KEY_Z:
           vga_add_char_to_frame_buffer(CHAR_Z_UP);
-          c = 'Z';
           break;
       case KEY_SPACE:
           vga_add_char_to_frame_buffer(CHAR_SPACE);
-          c = ' ';
           break;
       default:
           vga_add_char_to_frame_buffer(CHAR_SPACE); 
